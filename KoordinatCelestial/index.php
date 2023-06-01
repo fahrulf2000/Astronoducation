d560d0a1f08522ed8f7e9bc55f1f8a936a4e9ff9
fahrulf2000 <fahrulfirdaus25@gmail.com>
2023-06-01 21:47:09 +0700

    g

--- KoordinatCelestial/index.php
+++ KoordinatCelestial/index.php
@@ -1,6 +1,7 @@
 <html>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <label>pilih Object</label><br>
-<select name="" id="">
+<select name="Nama" id="Nama" onchange="detail()">
 <?php
     include "koneksi.php";
 
@@ -15,9 +16,28 @@
 <br>
 
 <label>Asensiorekta</label><br>
-<input type="text" name="" id="">
+<input type="text" name="vasensiorekta" id="vasensiorekta">
 <br>
 
 <label>Deklinasi</label><br>
-<input type="text" name="" id="">
+<input type="text" name="vdeklinasi" id="vdeklinasi">
+<br>
+
+<script>
+    function detail(){
+        var id = $("#Nama").val();
+        $.ajax({
+            url :"data.php",
+            method :"POST",
+            data : {id:id},
+            dataType : "json",
+            success:function(data){
+                $('#vasensiorekta').val(data.Asensiorekta);
+                $('#vdeklinasi').val(data.Deklinasi);
+                
+            }
+        })
+    }
+</script>
+
 </html>
--- KoordinatCelestial/koneksi.php
+++ KoordinatCelestial/koneksi.php
@@ -1,6 +1,3 @@
 <?php
 $koneksi = mysqli_connect("localhost","root","","tb_koordinat")
-if(!$koneksi){
-    echo "koneksi gagal"
-}
 ?>
